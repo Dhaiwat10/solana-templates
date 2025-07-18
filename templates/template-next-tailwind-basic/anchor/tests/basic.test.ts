74050e913223fbb0e3a6471aed35c16f61424793
dhai <dhaiwatpandya@gmail.com>
2025-07-18 13:50:05 +0530

    Add gill-based Anchor tests

--- templates/template-next-tailwind-basic/anchor/tests/basic.test.ts
+++ templates/template-next-tailwind-basic/anchor/tests/basic.test.ts
@@ -1,7 +1,41 @@
+import fs from 'fs'
+import os from 'os'
+import path from 'path'
+import {
+  appendTransactionMessageInstruction,
+  createSolanaClient,
+  createTransactionMessage,
+  pipe,
+  setTransactionMessageFeePayerSigner,
+  setTransactionMessageLifetimeUsingBlockhash,
+  signAndSendTransactionMessageWithSigners,
+} from 'gill'
+import { loadKeypairSignerFromFile } from 'gill/node'
+import { getGreetInstruction } from '@project/anchor'
+
 describe('basic', () => {
-  // TODO: Implement tests for the basic program based on the Codama generated client.
-  // Use tests in `legacy/legacy-next-tailwind-basic/anchor/tests/basic.test.ts` as a reference.
+  const anchorToml = fs.readFileSync(path.resolve(__dirname, '..', 'Anchor.toml'), 'utf8')
+  const walletMatch = anchorToml.match(/wallet\s*=\s*"([^"]+)"/)
+  const walletPath = walletMatch ? walletMatch[1].replace(/^~(?=\/)/, os.homedir()) : ''
+
+  const client = createSolanaClient({ urlOrMoniker: 'localnet' })
+  let signer: Awaited<ReturnType<typeof loadKeypairSignerFromFile>>
+
+  beforeAll(async () => {
+    signer = await loadKeypairSignerFromFile(walletPath)
+  })
+
   it('should run the program and print "GM!" to the transaction log', async () => {
-    expect(true).toBe(true)
+    const { value: latestBlockhash } = await client.rpc.getLatestBlockhash().send()
+    const message = pipe(
+      createTransactionMessage({ version: 0 }),
+      (m) => setTransactionMessageFeePayerSigner(signer, m),
+      (m) => setTransactionMessageLifetimeUsingBlockhash(latestBlockhash, m),
+      (m) => appendTransactionMessageInstruction(getGreetInstruction(), m),
+    )
+
+    const signature = await signAndSendTransactionMessageWithSigners(message)
+    const tx = await client.rpc.getTransaction(signature).send()
+    expect(tx.meta?.logMessages?.some((l) => l.includes('GM!'))).toBe(true)
   })
 })
